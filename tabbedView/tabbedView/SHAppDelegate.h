aa6e2a3946c93619d897d4bc8c7e1dec0f45a900
Jason Macbook Air <jason@jasonmcdermott.net>
2014-01-08 13:27:58 +1100

    new tabbed & navigation view example.

---
+++ tabbedView/tabbedView/SHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SHAppDelegate.h
+//  tabbedView
+//
+//  Created by Jason McDermott on 8/01/2014.
+//  Copyright (c) 2014 Sensorium Health. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tabbedView/tabbedView/SHViewController.m
@@ -0,0 +1,29 @@
+//
+//  SHViewController.m
+//  tabbedView
+//
+//  Created by Jason McDermott on 8/01/2014.
+//  Copyright (c) 2014 Sensorium Health. All rights reserved.
+//
+
+#import "SHViewController.h"
+
+@interface SHViewController ()
+
+@end
+
+@implementation SHViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
