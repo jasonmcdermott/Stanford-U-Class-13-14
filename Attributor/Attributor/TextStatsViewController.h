f4ba5d6b28c12907e2bd9dc6d247b1df912c514d
Jason Macbook Air <jason@jasonmcdermott.net>
2014-01-08 13:19:18 +1100

    New view controller, Navigation controller class, stats analysis on text. freaking awesome!

---
+++ Attributor/Attributor/TextStatsViewController.h
@@ -0,0 +1,15 @@
+//
+//  TextStatsViewController.h
+//  Attributor
+//
+//  Created by Jason McDermott on 8/01/2014.
+//  Copyright (c) 2014 Sensorium Health. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatsViewController : UIViewController
+
+@property (nonatomic, strong) NSAttributedString *textToAnalyze;
+
+@end
